7c17dea851f345c04a7aea68d66bcd2f5deada2b
ciprian <ciprian@drunomics.com>
2025-03-31 18:31:49 +0300

    Add reverse proxy.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -4,3 +4,9 @@
 if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
   include $app_root . '/' . $site_path . '/settings.ddev.php';
 }
+
+use Symfony\Component\HttpFoundation\Request;
+
+$settings['reverse_proxy'] = TRUE;
+$settings['reverse_proxy_addresses'] = [$_SERVER['REMOTE_ADDR']];
+$settings['reverse_proxy_header'] = Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PROTO;
