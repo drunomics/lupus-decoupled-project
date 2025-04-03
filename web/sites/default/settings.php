1475a5aaf74446dd5509f1dea27f984e82e07a9f
Liopold D. Novelli <liopold@drunomics.com>
2025-04-03 13:28:02 +0200

    build: MCD-398: Add secrets to devcontainers, add comments for reverse proxy.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -7,6 +7,10 @@ if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('
 
 use Symfony\Component\HttpFoundation\Request;
 
+// @todo: This is a temporary workaround to fix the View API Output operation
+// on nodes. Without reverse proxy the View API Output is redirecting to
+// http://localhost:80 instead of the backend URL of the current GitHub
+// Codespace.
 $settings['reverse_proxy'] = TRUE;
 $settings['reverse_proxy_addresses'] = [$_SERVER['REMOTE_ADDR']];
 $settings['reverse_proxy_header'] = Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PROTO;
